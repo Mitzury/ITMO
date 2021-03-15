aada5825713db1aad7bd844d6cd362b2db5bdc03
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-03-15 18:57:37 +0300

    Перезалив

---
+++ CSharp/ConsoleLab/LAB3/WhatDay1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDay
+{
+    class Program
+    {
+        enum MonthName { Januaru, February, Marth, Aptil, May, June, July, August, September, October, November, December };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter the year: ");
+                string line = Console.ReadLine();
+                int yearNum = int.Parse(line);
+                bool isLeapYear = (yearNum % 4 == 0);
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine(" IS a leap year");
+                }
+                else
+                {
+                    Console.WriteLine(" is NOT a leap year");
+                }
+                int maxDayNum = isLeapYear ? 366 : 365;
+
+                ArrayList DaysInMonths = new ArrayList() { 31, 28, 31, 30, 31, 31, 31, 30, 31, 30, 30, 31 };
+                ArrayList DaysInMonthsLeap = new ArrayList() { 31, 29, 31, 30, 31, 31, 31, 30, 31, 30, 30, 31 };
+                Console.WriteLine("Введите день от 1 до 365 или 366 если год високосный");
+                string day = Console.ReadLine();
+                int Day = int.Parse(day);
+                if (Day < 1 || Day > maxDayNum)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+                int MonthNam = 0;
+                if (isLeapYear)
+                    foreach (int daysInMonth in DaysInMonthsLeap)
+                    {
+                        if (Day <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Day -= daysInMonth;
+                            MonthNam++;
+                        }
+                    }
+                else
+                    foreach (int daysInMonth in DaysInMonths)
+                    {
+                        if (Day <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Day -= daysInMonth;
+                            MonthNam++;
+                        }
+                    }
+                MonthName temp = (MonthName)MonthNam;
+                string MonthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", Day, MonthName);
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ CSharp/ConsoleLab/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Lesson
+{
+    class Program
+    {
+        static int GetAge()
+        {
+            Random rnd = new Random();
+            //Получить случайное число 
+            int rage = rnd.Next(18, 88);
+            //Вернуть полученное значение
+            return rage;
+        }
+        static int GetHeight()
+        {
+            Random rnd2 = new Random();
+            //Получить случайное число 
+            int rheight = rnd2.Next(150, 250);
+            //Вернуть полученное значение
+            return rheight;
+        }
+        static void Main(string[] args)
+        {
+            //Приветствие
+            Console.WriteLine("Добро пожаловать в программу поиска девушки");
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+            // Вопрос 1.
+            Console.WriteLine("Введите желаемый рост девушки (150 - 250)");
+            int height = int.Parse(Console.ReadLine());
+            // Вопрос 2.
+            Console.WriteLine("Введите желаемый возраст девушки (18-88)");
+            int age = int.Parse(Console.ReadLine());
+            // Итог
+            Console.WriteLine("Ваш выбор, девушка с ростом: " + height + " и возрастом: " + age);
+            // Задаем цикл
+            Console.WriteLine("Введите количество раз, сколько будем искать (от 1 до бесконечности)");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            for (i = 1; i <= n; i++)
+            {
+                if (GetAge() == age && GetHeight() == height)
+                {
+                    Console.WriteLine("Ура, мы нашли для Вас девушку");
+                    Console.WriteLine("Число просмотренных девушек в базе: " + i + " из " + n
+
+                        );
+                    break;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, 12);
+                    Console.WriteLine("Ищем девушку, № в базе: " + i + " Текущий возраст: " + GetAge() + " Текущий рост:" + GetHeight());
+                }
+            }
+            Console.WriteLine("Увы не нашли");
+            //Конец
+            Console.ReadKey();
+        }
+    }
+}
