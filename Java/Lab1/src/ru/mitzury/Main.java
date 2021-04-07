494c94318f4ad74ba03e7d74f76947b957681db0
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-04-07 20:25:28 +0300

    Изменение

--- Java/Lab1/src/ru/mitzury/Main.java
+++ Java/Lab1/src/ru/mitzury/Main.java
@@ -1,10 +1,27 @@
 package ru.mitzury;
-import java.util.Arrays;
+
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+
+        boolean sorted = true;
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        if (nums.length <= 1) {
+            System.out.println("Array is sorted");
+        } else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+            System.out.println(sorted ? "ok" : "Please, try again");
+        }
+
     }
+
 }
