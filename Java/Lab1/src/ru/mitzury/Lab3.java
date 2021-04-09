cb2d6ec9a9cbb8224ddfe953a9239c38fb5aabf0
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-04-09 16:39:28 +0300

    lab3

--- Java/Lab1/src/ru/mitzury/Lab3.java
+++ Java/Lab1/src/ru/mitzury/Lab3.java
@@ -1,15 +1,15 @@
 package ru.mitzury;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab3 {
-
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     public static void lab31() {
-
-System.out.println("Напишите программу, которая проверяет отсортирован ли массив по возрастанию. " +
-"Если он отсортирован по возрастанию то выводится “OK”, если нет, " +
-"то будет выводиться текст “Please, try again”" + "\n");
+        System.out.println("Напишите программу, которая проверяет отсортирован ли массив по возрастанию. " +
+        "Если он отсортирован по возрастанию то выводится “OK”, если нет, " +
+        "то будет выводиться текст “Please, try again”" + "\n");
 
         boolean sorted = true;
         int[] nums = new int[]{1, 2, 3, 10, 5};
@@ -24,8 +24,28 @@ System.out.println("Напишите программу, которая пров
             }
             System.out.println(sorted ? "ok" : "Please, try again");
         }
-
     }
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public static void lab32() {
+        System.out.println("Напишите программу, которая считывает с клавиатуры длину массива " +
+                "(например, пользователь вводит цифру 4), затем пользователь вводит 4 числа и на" +
+                " новой строке выводится массив из 4 элементов." + "\n");
+
+        System.out.println("Array length: ");
+        Scanner scanner = new Scanner(System.in);
+        int aLenght = scanner.nextInt();
+        int[] nums = new int[aLenght];
+        for (int i = 0; i < aLenght; i++) {
+            System.out.println("Enter numbers of array: ");
+            int num = scanner.nextInt();
+            nums[i]  = num;
+        }
+        String intArrayString = Arrays.toString(nums);
+        System.out.println(intArrayString);
+    }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 }
 
 
--- Java/Lab1/src/ru/mitzury/Main.java
+++ Java/Lab1/src/ru/mitzury/Main.java
@@ -1,16 +1,13 @@
 package ru.mitzury;
-
-
 import java.util.Scanner;
-
-import static ru.mitzury.Lab3.lab31;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-       lab31();
+       Lab3.lab32();
+
 
 
     }
