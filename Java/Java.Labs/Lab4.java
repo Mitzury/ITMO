ae6ce8beae9fdc3440769c2173f6cc71bd9a36ce
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-04-19 13:10:29 +0300

    1

--- Java/Java.Labs/Lab4.java
+++ Java/Java.Labs/Lab4.java
@@ -29,4 +29,36 @@ public class Lab4 {
             System.out.println(word + " - это слово не палиндром.");
         }
     }
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public static void lab43() {
+        System.out.println("Напишите метод, заменяющий в тексте все вхождения слова «бяка» на «[вырезано цензурой]»" + "\n");
+
+        String str1 = "Говорить бяка плохо.";
+        String str2 = str1.replace("бяка", "[вырезано цензурой]");
+        System.out.println("Было: " + str1);
+        System.out.println("Стало: " + str2);
+    }
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public static void lab44() {
+        System.out.println("Имеются две строки. Найти количество вхождений одной (являющейся подстрокой) в другую." + "\n");
+        String str1 = "Первая строка.";
+        String str2 = "Вторая строка.";
+        int i = str1.split(str2).length;
+        System.out.println("Количество вхождений: " + i);
+
+    }
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public static void lab45() {
+        System.out.println("Напишите метод, который инвертирует слова в строке. Предполагается, что в строке нет знаков препинания, и слова разделены пробелами.\n" +
+                "Sample Output:\n" +
+                "The given string is: This is a test string\n" +
+                "The string reversed word by word is:\n" +
+                "sihT si a tset gnirts\n" + "\n");
+
+        String str1 = "This is test string";
+        System.out.println("Нормальная строка: " + str1);
+        String revStr = new StringBuilder(String.valueOf(str1)).reverse().toString();
+        System.out.println("Реверсивная строка: " + revStr);
+    }
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
