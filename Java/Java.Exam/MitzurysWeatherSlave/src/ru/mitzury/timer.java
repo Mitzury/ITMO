306ae9d9274870ec5498f271fbc6d04045bb966f
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-05-04 22:09:14 +0300

    simple update

--- Java/Java.Exam/MitzurysWeatherSlave/src/ru/mitzury/timer.java
+++ Java/Java.Exam/MitzurysWeatherSlave/src/ru/mitzury/timer.java
@@ -6,13 +6,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class timer {
-
     public void run() {
         System.out.println("TimerTask начал свое выполнение в:" + new Date());
         completeTask();
         System.out.println("TimerTask закончил свое выполнение в:" + new Date());
     }
-
     private void completeTask() {
         try {
             // допустим, выполнение займет 20 секунд
