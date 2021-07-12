55f67182556b704212015ffd244910725b2eeefc
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-07-12 17:56:37 +0300

    Добавил второе задание.

---
+++ 8. C++/Labs/Lab1/Lab2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Определение високовного года\n";
+    std::cout << "Введите год yyyy:\n";
+    int year;
+    std::cin >> year;
+    std::cout << "Вы ввели: " << year;
+
+    if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
+    {
+        std::cout << "Год был високосный:\n";
+    }
+    else
+        std::cout << "Год был не високосный\n";
+}
