3169298c60decce0f71e23b133918a755b80fc5e
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-07-14 16:39:19 +0300

    1

--- 8. C++/Labs/Lab3/Lab3.cpp
+++ 8. C++/Labs/Lab3/Lab3.cpp
@@ -3,6 +3,11 @@
 int main()
 {
     std::cout << "Программа вычисления площади сложной фигуры!\n";
+    std::cout << "Введите х!\n";
 
+}
+
+void sqrt_figure(double x, double y, double x1, double y1)
+{
 
 }
