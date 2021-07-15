7e57a6a3124b927756ab28426bd325390f712a5a
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-07-15 17:54:37 +0300

    fix

--- 8. C++/Labs/Lab2/Lab2.cpp
+++ 8. C++/Labs/Lab2/Lab2.cpp
@@ -2,77 +2,77 @@
 
 int main()
 {
-    //// Lab 2. Ex. 1
+    // Lab 2. Ex. 1
 
     setlocale(LC_ALL, "Russian");
-    //std::cout << "Определение високовного года\n";
-    //std::cout << "Введите год yyyy:\n";
-    //int year;
-    //std::cin >> year;
-    //std::cout << "Вы ввели: " << year;
+    std::cout << "Определение високовного года\n";
+    std::cout << "Введите год yyyy:\n";
+    int year;
+    std::cin >> year;
+    std::cout << "Вы ввели: " << year;
 
-    //if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
-    //{
-    //    std::cout << "Год был високосный:\n";
-    //}
-    //else
-    //    std::cout << "Год был не високосный\n";
+    if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
+    {
+        std::cout << "Год был високосный:\n";
+    }
+    else
+        std::cout << "Год был не високосный\n";
 
-    //
-    //// Lab 2. Ex. 2
+    
+    // Lab 2. Ex. 2
 
-    //std::cout << "Введите три числа и я скажу какое из них наибольшее.\n";
-    //int a;
-    //int b;
-    //int c;
+    std::cout << "Введите три числа и я скажу какое из них наибольшее.\n";
+    int a;
+    int b;
+    int c;
 
-    //std::cin >> a;
-    //std::cin >> b;
-    //std::cin >> c;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
 
-    //if (a > b and a > c)
-    //{
-    //    std::cout << a;
-    //}
-    //else if (b > a and b > c)
-    //{
-    //    std::cout << b;
-    //}
-    //else if (c > a and c > b)
-    //{
-    //    std::cout << c;
-    //}
+    if (a > b and a > c)
+    {
+        std::cout << a;
+    }
+    else if (b > a and b > c)
+    {
+        std::cout << b;
+    }
+    else if (c > a and c > b)
+    {
+        std::cout << c;
+    }
 
-    //// Lab 2. Ex. 3
+    // Lab 2. Ex. 3
 
-    //std::cout << "Задача о размене монет\n";
-    //std::cout << "Введите сумму которую Вам разменять: (x.xx) \n";
-    //double summ;
-    //std::cin >> summ;
+    std::cout << "Задача о размене монет\n";
+    std::cout << "Введите сумму которую Вам разменять: (x.xx) \n";
+    double summ;
+    std::cin >> summ;
 
-    //double coin10 = summ / 10;
-    //std::cout << "Монет по 10р.: " << trunc(coin10) << "\n";
-    //summ = summ - trunc(coin10) * 10;
-    //double coin5 = summ / 5;
-    //    
-    //std::cout << "Монет по 5р.: " << trunc(coin5) << "\n";
-    //summ = summ - trunc(coin5) * 5;
-    //double coin05 = summ / 0.5;
-    //    
-    //std::cout << "Монет по 50коп.: " << trunc(coin05) << "\n";
-    //summ = summ - trunc(coin05) * 0.5;
-    //double coin01 = summ / 0.1;
-    //    
-    //std::cout << "Монет по 10коп.: " << trunc(coin01) << "\n";
-    //summ = summ - trunc(coin01) * 0.1;
-    //double coin001 = summ / 0.01;
-    //    
-    //std::cout << "Монет по 1коп.: " << trunc(coin001) << "\n";
-    //summ = summ - coin001 * 0.01;
+    double coin10 = summ / 10;
+    std::cout << "Монет по 10р.: " << trunc(coin10) << "\n";
+    summ = summ - trunc(coin10) * 10;
+    double coin5 = summ / 5;
+        
+    std::cout << "Монет по 5р.: " << trunc(coin5) << "\n";
+    summ = summ - trunc(coin5) * 5;
+    double coin05 = summ / 0.5;
+        
+    std::cout << "Монет по 50коп.: " << trunc(coin05) << "\n";
+    summ = summ - trunc(coin05) * 0.5;
+    double coin01 = summ / 0.1;
+        
+    std::cout << "Монет по 10коп.: " << trunc(coin01) << "\n";
+    summ = summ - trunc(coin01) * 0.1;
+    double coin001 = summ / 0.01;
+        
+    std::cout << "Монет по 1коп.: " << trunc(coin001) << "\n";
+    summ = summ - coin001 * 0.01;
 
-    //std::cout << "Остаток: " << summ;
+    std::cout << "Остаток: " << summ;
 
-    // Lab 2. Ex. 4
+     Lab 2. Ex. 4
 
     std::cout << "Задача о стрелке и мишени\n";
     std::cout << "Укажите сложность игры: (0-5)\n";
@@ -109,9 +109,6 @@ int main()
             std::cout << "Вы попали в молоко \n";
             coinMilk = coinMilk + 1;
         }
-
-       
-        
     }
     std::cout << "\n Попаданий в яблочко: " << coin;
     std::cout << "\n Попаданий в середину: " << coinN;
@@ -125,8 +122,6 @@ int main()
         std::cout << "Середнячок \n";
     }
     else {
-        std::cout << " \n Надо больше тренироваться \n";
+        std::cout << " \n Надо больше тренироваться";
     }
-        
-
 }
