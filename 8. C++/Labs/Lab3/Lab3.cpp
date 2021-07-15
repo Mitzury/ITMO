50dc2f5ad0ffb40c7b03f30ca3154e9f683618d7
bogdanovsi@gmail.com <bogdanovsi@gmail.com>
2021-07-15 18:10:17 +0300

    Наработка 3.1 лабы

--- 8. C++/Labs/Lab3/Lab3.cpp
+++ 8. C++/Labs/Lab3/Lab3.cpp
@@ -2,12 +2,24 @@
 
 int main()
 {
-    std::cout << "Программа вычисления площади сложной фигуры!\n";
-    std::cout << "Введите х!\n";
+    std::cout << "Программа вычисления площади сложной фигуры! Пятиугольник.\n";
+    std::cout << "Введите координаты вершин пятиугольника\n";
+    int x, y, x1, y1;
 
+
+    for (int i = 1; i <= 5; i++)
+    {
+        std::cout << "Введите Х" << i << "й вершины\n ";
+        std::cin >> x;
+        std::cout << "Введите Y" << i << "й вершины\n ";
+        std::cin >> y;
+
+        sqrt_figure(x, y);
+    }
+ 
 }
 
-void sqrt_figure(double x, double y, double x1, double y1)
+void sqrt_figure(double x, double y)
 {
 
 }
